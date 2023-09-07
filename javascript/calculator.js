550c46502b27687c663d488f4ecb38c40a2356c7
David Singleton <davidjsingleton18@gmail.com>
2023-09-07 11:45:48 -0700

    Add basic way to display number inputs and ONLY number inputs

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -1,5 +1,39 @@
+// ---------------------------------------------------------------------------
+//
+//         APPLIES THE CLASSES TO THE HTML TO KEEP IT LESS CLUTTERED
+//
+// ---------------------------------------------------------------------------
+
 const calcButtonContainer = document.querySelector('.calc-btn-container');
+const calcButtonContainerTwo = document.querySelector(
+  '.calc-btn-container-two'
+);
 
 const calcButtons = Array.from(calcButtonContainer.children);
+const operatorButtons = Array.from(calcButtonContainerTwo.children);
 
 calcButtons.forEach((button) => button.classList.add('calc-btn'));
+
+operatorButtons.forEach((button) => button.classList.add('operator-btn'));
+
+// ---------------------------------------------------------------------------
+//
+//                          INPUT LOGIC FOR NUMBERS
+//
+// ---------------------------------------------------------------------------
+
+const firstInput = [];
+const secondInput = [];
+
+const isANumber = (number) => parseInt(number) <= 9;
+
+const calcDisplay = document.querySelector('.calc-display');
+
+calcButtons.forEach((calcButton) => {
+  calcButton.addEventListener('click', () => {
+    if (isANumber(calcButton.textContent)) {
+      firstInput.push(calcButton.textContent);
+      calcDisplay.textContent = firstInput.join('');
+    }
+  });
+});
