cf39d74d96f0a03575ec22a41f41646ba4bf618b
David Singleton <davidjsingleton18@gmail.com>
2023-09-08 16:19:20 -0700

    Add object array for cleaner reading of button input

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -25,19 +25,47 @@ operatorButtons.forEach((button) => button.classList.add('operator-btn'));
 const firstInput = [];
 const secondInput = [];
 
-const isButtonValid = (number) => {
-  if (parseInt(number) <= 9 || (number === '.' && !firstInput.includes('.'))) {
-    return number;
-  }
+const calcInputs = {
+  9: 1,
+  10: 2,
+  11: 3,
+  6: 4,
+  7: 5,
+  8: 6,
+  3: 7,
+  4: 8,
+  5: 9,
+  12: '0',
+  13: '00',
+  14: '.',
 };
 
 const calcDisplay = document.querySelector('.calc-display');
 
-calcButtons.forEach((calcButton) => {
+// calcButtons.forEach((calcButton) => {
+//   calcButton.addEventListener('click', () => {
+//     if (isButtonValid(calcButton.textContent)) {
+//       firstInput.push(calcButton.textContent);
+//       calcDisplay.textContent = firstInput.join('');
+//     }
+//   });
+// });
+
+calcButtons.forEach((calcButton, index) => {
   calcButton.addEventListener('click', () => {
-    if (isButtonValid(calcButton.textContent)) {
-      firstInput.push(calcButton.textContent);
+    const buttonIndex = calcInputs[index];
+    // eslint-disable-next-line no-use-before-define
+    if (isInputValid(buttonIndex)) {
+      firstInput.push(buttonIndex);
       calcDisplay.textContent = firstInput.join('');
     }
   });
 });
+
+const isInputValid = (number) => {
+  if (number <= 9 || (number === '.' && !firstInput.includes('.'))) {
+    return number;
+  }
+
+  return false;
+};
